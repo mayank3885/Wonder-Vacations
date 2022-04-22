2bfdd43f20cd3d9719885488db517cdfdfdeaa95
mayank makwana <tinythanos8@gmail.com>
2022-04-22 20:24:18 +0530

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/pages/Home.jsx";
@@ -11,12 +11,12 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/packages" exact component={Packages} />
-          <Route path="/contact" exact component={Contacts} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/packages" element={<Packages/>} />
+          <Route path="/contact" element={<Contacts/>} />
+        </Routes>
       </Router>
     </div>
   );
